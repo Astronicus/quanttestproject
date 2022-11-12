4b30ace93dbab065160ff5664e05c78faa3009c6
Cameron Weiner <90437273+LeakedYew@users.noreply.github.com>
2022-11-12 14:33:41 -0600

    Update index.tsx

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -11,27 +11,4 @@ const Home: NextPage = () => {
     </PageLayout>
   );
 };
-
-class Prompt_Box  
-{  
- show()  
- {  
-  var promptValue = prompt('Enter any value in below', '');  
-  if (promptValue != null) {  
-   document.getElementById("Status").innerHTML = "Prompt Box value ->" + promptValue;  
-  } else  
-  {  
-   document.getElementById("Status").innerText = "You cancel Prompt Box";  
-  }  
- }  
-}  
-window.onload = () =>  
- {  
-  var bttn = < HTMLButtonElement > document.getElementById("Button1");  
-  bttn.onclick = function()  
-  {  
-   var obj = new Prompt_Box();  
-   obj.show();  
-  }  
- };  
 export default Home;
